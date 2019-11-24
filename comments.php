d355f2a98cf7983702568b05453438686f3c6e83
andrei17365 <andrei17365@mail.ru>
2019-11-24 18:44:53 +0200

    first commit

---
+++ comments.php
@@ -0,0 +1,33 @@
+<?php
+	$driver = 'mysql'; // тип базы данных, с которой мы будем работать
+
+	$host = 'localhost';// альтернатива '127.0.0.1' - адрес хоста, в нашем случае локального
+
+	$db_name = 'projectphp1'; // имя базы данных
+
+	$db_user = 'root'; // имя пользователя для базы данных
+
+	$db_password = ''; // пароль пользователя
+
+	$charset = 'utf8'; // кодировка по умолчанию
+
+	$options = [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]; // массив с дополнительными настройками подключения. В данном примере мы установили отображение ошибок, связанных с базой данных, в виде исключений
+
+	$dsn = "$driver:host=$host;dbname=$db_name;charset=$charset";
+
+	$pdo = new PDO($dsn, $db_user, $db_password, $options);
+
+	$sql = "INSERT INTO comments (name, text) VALUES (:name, :text)";
+
+	$statement = $pdo->prepare($sql);
+
+	$statement->bindParam(':name', $name);
+	$statement->bindParam(':text', $text);
+	$name = $_POST['name'];
+	$text = $_POST['text'];
+	$statement->execute();
+
+	header('Location: /index.php');
+
+	var_dump($_POST);
+?>
